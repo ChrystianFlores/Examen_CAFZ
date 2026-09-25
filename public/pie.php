0cf432c5e6698735ca62048dc8cefdeb3763aa39
Chrystian Flores <alonsofz2005@gmail.com>
2026-09-25 09:53:48 -0600

    Arreglos

--- public/pie.php
+++
@@ -1,5 +0,0 @@
-<!-- Pie de página del sitio -->
-<footer class="pie">
-    <!-- Texto de derechos con el año actual generado por PHP -->
-    <p class="pie-texto">&copy; <?php echo date('Y'); ?> Examen CAFZ. Todos los derechos reservados.</p>
-</footer>
